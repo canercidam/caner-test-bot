b96cb620842dee551115c1b747c7eb99c6922f36
Caner Çıdam <canercidam01@gmail.com>
2023-07-18 13:19:17 +0200

    add nop bot

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 .PHONY: build
 build:
-	@CGO_ENABLED=0 go build -o bot .
-	@docker build -t disco.forta.network/caner-test-bot .
+	@CGO_ENABLED=0 go build -o build/bot .
+	@docker build -t disco.forta.network/caner-test-bot -f Dockerfile ./build
+	@docker push disco.forta.network/caner-test-bot
+
+.PHONY: build-nop
+build-nop:
+	@CGO_ENABLED=0 go build -o build/bot ./nop
+	@docker build -t disco.forta.network/caner-test-bot -f Dockerfile ./build
 	@docker push disco.forta.network/caner-test-bot
---
+++ nop/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/forta-network/forta-core-go/protocol"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:50051"))
+	if err != nil {
+		panic(err)
+	}
+	server := grpc.NewServer()
+
+	protocol.RegisterAgentServer(server, &agentServer{})
+
+	log.Println("Starting agent server...")
+	log.Println(server.Serve(lis))
+}
+
+type agentServer struct {
+	protocol.UnimplementedAgentServer
+}
+
+func (as *agentServer) Initialize(context.Context, *protocol.InitializeRequest) (*protocol.InitializeResponse, error) {
+	return &protocol.InitializeResponse{
+		Status: protocol.ResponseStatus_SUCCESS,
+	}, nil
+}
+
+func (as *agentServer) EvaluateTx(ctx context.Context, txRequest *protocol.EvaluateTxRequest) (*protocol.EvaluateTxResponse, error) {
+	return &protocol.EvaluateTxResponse{
+		Status: protocol.ResponseStatus_SUCCESS,
+	}, nil
+}
+
+func (as *agentServer) EvaluateBlock(ctx context.Context, req *protocol.EvaluateBlockRequest) (*protocol.EvaluateBlockResponse, error) {
+	return &protocol.EvaluateBlockResponse{
+		Status: protocol.ResponseStatus_SUCCESS,
+	}, nil
+}
